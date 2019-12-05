31861e1a9a0f5060e0abc58f71aab8c9ef9daf25
zsh <1764588008@qq.com>
2019-12-05 20:31:00 +0800

    '导航及首页'

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+// 懒加载
+const Home = () => import ('views/home/Home')
+const Category = () => import ('views/category/Category')
+const Cart = () => import ('views/cart/Cart')
+const Profile = () => import ('views/profile/Profile')
+
+// 1.通过 Vue.use(插件),安装插件
+Vue.use(VueRouter)
+
+// 2.创建Router对象
+const router = new VueRouter({
+    routes: [
+        {
+            path: '',
+            redirect: '/home'
+        },
+        {
+            path: '/home',
+            component: Home
+        },
+        {
+            path: '/category',
+            component: Category
+        },
+        {
+            path: '/cart',
+            component: Cart
+        },
+        {
+            path: '/profile',
+            component: Profile
+        }
+    ],
+    mode: 'history'
+})
+
+export default router
